cc2795d28024c51d4ae8d0fa0a77a7e8263e3cee
xiu88124 <72498380+xiu88124@users.noreply.github.com>
2022-08-19 10:51:45 +0800

    Update h1_to_Internet_del.sh

--- h1_to_Internet_del.sh
+++ h1_to_Internet_del.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ifconfig enp0s8 0
-nmcli con mod enp0s8 ipv4.addresses 172.30.5.88/24
-nmcli con mod enp0s8 ipv4.gateway 172.30.5.1
+ip netns del ns0
+ip link set br0 down
+brctl delbr br0
+
