49aa6871dd05c81a36ee0919af2cd1ad68c71a76
xiu88124 <72498380+xiu88124@users.noreply.github.com>
2022-09-29 15:02:25 +0800

    Delete turn_to_enp0s8.sh

--- turn_to_enp0s8.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ifmetric enp0s8 0
-ifmetric enp0s3 1
-iptables -t nat --delete POSTROUTING 1
-iptables -t nat -A POSTROUTING -s 192.168.15.0/24 -o enp0s8 -j MASQUERADE
-ip netns exec ns0 ping 8.8.8.8
